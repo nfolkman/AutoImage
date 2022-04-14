71de4c9869e5fb0febba526868c7f76bd2b0aa75
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-04-14 16:49:06 -0500

    api key security

---
+++ GrabAnImageAPI/config.js
@@ -0,0 +1,3 @@
+const config = {
+    APIKey: 'vHNE72VielZI95_00dq6DHySEKNCd7yIajiczXbGwLw',
+}
--- GrabAnImageAPI/main.js
+++ GrabAnImageAPI/main.js
@@ -1,11 +1,13 @@
 
+const apiKey = config.APIKey;
+
 document.querySelector('button').addEventListener('click', getImage)   // event listener for click input
 
 
 function getImage(){
    
       
-      fetch("https://api.unsplash.com/photos/random?client_id=vHNE72VielZI95_00dq6DHySEKNCd7yIajiczXbGwLw")
+      fetch("https://api.unsplash.com/photos/random?client_id=" + apiKey)
          .then(res => res.json()) // parse response as json
          .then(data => {
             console.log(data.urls.regular)
